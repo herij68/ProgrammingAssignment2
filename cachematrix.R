05dc6398dca60b7c7360afdf3bbdff167ed23a6d
herij68 <45743159+herij68@users.noreply.github.com>
2018-12-19 10:25:48 +0000

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,34 +1,33 @@
-## Put comments here that give an overall description of what your
-## functions do
 
-## Write a short comment describing this function
+## The makeCacheMatrix function ultimately returns a list of the inverse.
 
-makeCacheMatrix <- function(x = matrix()) {
-        m <- NULL
-        set <- function(y) {
-                x <<- y
-                m <<- NULL
+makeCacheMatrix <- function(x = matrix()) {   #Default of argument x is an empty matrix.
+        m <- NULL                             #Creates inv, an empty object to be used later.
+        set <- function(y) {                  # function with argument y
+                x <<- y                       # y is assigned to x, the matrix in parent eniviroment of makeCacheMatrix.
+                m <<- NULL                    # NULL is assigned to inv, also in parent enviroment.
         }
-        get <- function() x
-        setsolve <- function(solve) m <<- solve
-        getsolve <- function() m
+        get <- function() x                   # x is not defined within function get(), therefore R retrive it from parent enivorment.
+        setsolve <- function(solve)           # setter function for the inverse.
+            m <<- solve                       # inv is in parent enivorment.
+        getsolve <- function() m              # R retrive the value of inv from parent enivorment.
         list(set = set, get = get,
              setsolve = setsolve,
-             getsolve = getsolve)
+             getsolve = getsolve)             # Finally, a list is returned.
 }
 
+## cacheSolve computes the inverse of the list that is returned from makeCacheMAtrix.
+## If the inverse has been computed already, then this function will get the inverse from the cache.
+## This speed things up.
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-        m <- x$getsolve()
-        if(!is.null(m)) {
-                message("getting cached data")
-                return(m)
+cacheSolve <- function(x, ...) {              # argument x refers to the list makeCacheMatrix returns.
+        m <- x$getsolve()                     # Return a matrix that is the inverse of 'x'
+        if(!is.null(m)) {                     # If the result is NULL..
+              message("getting cached data")
+              return(m)                       # ...then return the value inv to parent enviroment.
         }
-        data <- x$get()
-        m <- solve(data, ...)
+        data <- x$get()                       # If !is.null is FALSE, the function get x.
+        m <- solve(data, ...)                 # And calucalte the inverse.
         x$setsolve(m)
-        m
+        m                                     # Returns the inverse.
 }
